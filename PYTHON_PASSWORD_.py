d61e4b4642d8b3a987602460823f64ce9bd64b10
cunitza <fiodor928@gmail.com>
2025-03-05 15:40:21 +0300

    py3

---
+++ PYTHON_PASSWORD_.py
@@ -0,0 +1,14 @@
+# <yes> <report> PYTHON_PASSWORD_EMPTY 607f01
+password = ""
+password = "123"
+password = "123444"
+# <yes> <report> PYTHON_PASSWORD_NULL c37c10
+password = None
+# <yes> <report> PYTHON_PASSWORD_HARDCODED eed164
+password = "hardcoded"
+# <yes> <report> PYTHON_PASSWORD_HARDCODED eed164
+psswd = "also_hardcoded"
+
+pwd = "not_so_hardcoded"
+# <no> <report>
+password = pwd
